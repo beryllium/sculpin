2259144ac9c38dcc238217a63f25af6f3da88b8a
Kevin Boyd <kevin.boyd@gmail.com>
2026-07-03 14:20:38 -0700

    Adding an additional test case to demonstrate the complexity of the issue

--- src/Sculpin/Tests/Functional/ComplexProviderLifecycleTest.php
+++ src/Sculpin/Tests/Functional/ComplexProviderLifecycleTest.php
@@ -27,6 +27,65 @@ class ComplexProviderLifecycleTest extends FunctionalTestCase
         $this->executeSculpin(['generate']);
 
         $this->assertProjectHasGeneratedFile('/' . $expectedFile);
+
+        // @todo This assertion will fail, demonstrating a lifecycle problem: the inner include's content blocks are not rendered.
         $this->assertGeneratedFileHasContent('/' . $expectedFile, $expectedFileContents);
+    }
+
+    /** @test */
+    public function testComplexLifecycleBuildsProperly_WhileWatching(): void
+    {
+        $sourcePage = __DIR__ . self::PROJECT_DIR . '/source/_sections/1-jacksons.html';
+        $generatedPage = __DIR__ . self::PROJECT_DIR . '/' . 'output_test/index.html';
+        $expectedWord = 'hypothetical';
+        $matchString = "{% include 'jackson.html' %}";
+
+        // ensure consistent test state
+        $rawPage = file_get_contents($sourcePage);
+        $this->modifySourcePage(
+            filePath: $sourcePage,
+            regex: '# *' . $matchString . ' *#',
+            replacement: $matchString,
+            body: $rawPage
+        );
+
+        // start our async sculpin watcher/server
+        $process = $this->executeSculpinAsync(['generate', '--watch']);
+
+        sleep(1); // wait until our file exists
+        $pageContent = file_get_contents($generatedPage);
+
+        // check for the word being tested
+        // @todo If the below assertion is commented out, this test will PASS, demonstrating a lifecycle problem.
+        $this->assertStringContainsString($expectedWord, $pageContent);
+
+        // update the files under test by adding whitespace
+        $this->modifySourcePage(
+            filePath: $sourcePage,
+            regex: '#' . $matchString . '#',
+            replacement: '  ' . $matchString . ' ',
+            body: $rawPage
+        );
+
+        sleep(2);
+        $pageContent = file_get_contents($generatedPage);
+
+        // check for the word being tested
+        $this->assertStringContainsString($expectedWord, $pageContent);
+
+        $process->stop(0);
+
+        // reset the source page
+        $this->modifySourcePage(
+            filePath: $sourcePage,
+            regex: '# *' . $matchString . ' *#',
+            replacement: $matchString,
+            body: $rawPage
+        );
+    }
+
+    protected function modifySourcePage(string $filePath, string $regex, string $replacement, string $body): void
+    {
+        file_put_contents($filePath, preg_replace($regex, $replacement, $body));
     }
 }
