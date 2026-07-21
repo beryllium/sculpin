d2dbebd04310e6f31afd615e1a201992a6271807
Kevin Boyd <kevin.boyd@gmail.com>
2026-07-21 09:18:46 -0700

    Add legacy bodge to AbstractKernel to support php 81 / symfony 6.4

--- src/Sculpin/Bundle/SculpinBundle/HttpKernel/AbstractKernel.php
+++ src/Sculpin/Bundle/SculpinBundle/HttpKernel/AbstractKernel.php
@@ -22,156 +22,163 @@ use Symfony\Component\HttpKernel\Kernel;
 /**
  * @author Beau Simensen <beau@dflydev.com>
  */
-abstract class AbstractKernel extends Kernel
-{
-    protected $missingSculpinBundles = [];
-    protected $outputDir;
-    protected $projectDir;
-    protected $sourceDir;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct(string $environment, bool $debug, array $overrides = [])
+if (version_compare(PHP_VERSION, '8.2.0', '>=')) {
+    abstract class AbstractKernel extends Kernel
     {
-        $this->projectDir = $overrides['projectDir'] ?? null;
-        $this->outputDir  = $overrides['outputDir']  ?? null;
-        $this->sourceDir  = $overrides['sourceDir']  ?? null;
+        protected $missingSculpinBundles = [];
+        protected $outputDir;
+        protected $projectDir;
+        protected $sourceDir;
 
-        parent::__construct($environment, $debug);
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function __construct(string $environment, bool $debug, array $overrides = [])
+        {
+            $this->projectDir = $overrides['projectDir'] ?? null;
+            $this->outputDir  = $overrides['outputDir']  ?? null;
+            $this->sourceDir  = $overrides['sourceDir']  ?? null;
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function getKernelParameters(): array
-    {
-        return array_merge(parent::getKernelParameters(), [
-            'sculpin.project_dir'         => $this->projectDir,
-            'sculpin.output_dir_override' => $this->outputDir,
-            'sculpin.source_dir_override' => $this->sourceDir,
-        ]);
-    }
+            parent::__construct($environment, $debug);
+        }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function registerBundles(): array
-    {
-        $bundles = [
-            new \Sculpin\Bundle\StandaloneBundle\SculpinStandaloneBundle,
-            new \Sculpin\Bundle\MarkdownBundle\SculpinMarkdownBundle,
-            new \Sculpin\Bundle\TextileBundle\SculpinTextileBundle,
-            new \Sculpin\Bundle\MarkdownTwigCompatBundle\SculpinMarkdownTwigCompatBundle,
-            new \Sculpin\Bundle\PaginationBundle\SculpinPaginationBundle,
-            new \Sculpin\Bundle\SculpinBundle\SculpinBundle,
-            new \Sculpin\Bundle\ThemeBundle\SculpinThemeBundle,
-            new \Sculpin\Bundle\TwigBundle\SculpinTwigBundle,
-            new \Sculpin\Bundle\ContentTypesBundle\SculpinContentTypesBundle,
-            new \Sculpin\Bundle\PostsBundle\SculpinPostsBundle,
-        ];
+        /**
+         * {@inheritdoc}
+         */
+        protected function getKernelParameters(): array
+        {
+            return array_merge(parent::getKernelParameters(), [
+                'sculpin.project_dir'         => $this->projectDir,
+                'sculpin.output_dir_override' => $this->outputDir,
+                'sculpin.source_dir_override' => $this->sourceDir,
+            ]);
+        }
 
-        foreach ($this->getAdditionalSculpinBundles() as $class) {
-            if (class_exists($class)) {
-                $bundles[] = new $class();
-            } else {
-                $this->missingSculpinBundles[] = $class;
+        /**
+         * {@inheritdoc}
+         */
+        public function registerBundles(): array
+        {
+            $bundles = [
+                new \Sculpin\Bundle\StandaloneBundle\SculpinStandaloneBundle,
+                new \Sculpin\Bundle\MarkdownBundle\SculpinMarkdownBundle,
+                new \Sculpin\Bundle\TextileBundle\SculpinTextileBundle,
+                new \Sculpin\Bundle\MarkdownTwigCompatBundle\SculpinMarkdownTwigCompatBundle,
+                new \Sculpin\Bundle\PaginationBundle\SculpinPaginationBundle,
+                new \Sculpin\Bundle\SculpinBundle\SculpinBundle,
+                new \Sculpin\Bundle\ThemeBundle\SculpinThemeBundle,
+                new \Sculpin\Bundle\TwigBundle\SculpinTwigBundle,
+                new \Sculpin\Bundle\ContentTypesBundle\SculpinContentTypesBundle,
+                new \Sculpin\Bundle\PostsBundle\SculpinPostsBundle,
+            ];
+
+            foreach ($this->getAdditionalSculpinBundles() as $class) {
+                if (class_exists($class)) {
+                    $bundles[] = new $class();
+                } else {
+                    $this->missingSculpinBundles[] = $class;
+                }
+            }
+
+            return $bundles;
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function registerContainerConfiguration(LoaderInterface $loader)
+        {
+            // Load defaults.
+            $loader->load(__DIR__.'/../Resources/config/kernel.yml');
+
+            if (file_exists($file = $this->getProjectDir().'/app/config/sculpin_kernel_'.$this->getEnvironment().'.yml')) {
+                $loader->load($file);
+            } elseif (file_exists($file = $this->getProjectDir().'/app/config/sculpin_kernel.yml')) {
+                $loader->load($file);
             }
         }
 
-        return $bundles;
-    }
+        /**
+         * {@inheritdoc}
+         */
+        public function boot(): void
+        {
+            if (true === $this->booted) {
+                return;
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        // Load defaults.
-        $loader->load(__DIR__.'/../Resources/config/kernel.yml');
+            parent::boot();
 
-        if (file_exists($file = $this->getProjectDir().'/app/config/sculpin_kernel_'.$this->getEnvironment().'.yml')) {
-            $loader->load($file);
-        } elseif (file_exists($file = $this->getProjectDir().'/app/config/sculpin_kernel.yml')) {
-            $loader->load($file);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function boot(): void
-    {
-        if (true === $this->booted) {
-            return;
+            $this->container->compile();
         }
 
-        parent::boot();
+        /**
+         * {@inheritdoc}
+         */
+        protected function buildContainer(): ContainerBuilder
+        {
+            $container = $this->getContainerBuilder();
+            $container->addObjectResource($this);
+            $this->prepareContainer($container);
 
-        $this->container->compile();
-    }
+            if (file_exists($this->getProjectDir().'/app/config/sculpin_services.yml')) {
+                $loader = new YamlFileLoader($container, new FileLocator($this->getProjectDir().'/app/config'));
+                $loader->load('sculpin_services.yml');
+            }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function buildContainer(): ContainerBuilder
-    {
-        $container = $this->getContainerBuilder();
-        $container->addObjectResource($this);
-        $this->prepareContainer($container);
+            if (null !== $cont = $this->registerContainerConfiguration($this->getContainerLoader($container))) {
+                $container->merge($cont);
+            }
 
-        if (file_exists($this->getProjectDir().'/app/config/sculpin_services.yml')) {
-            $loader = new YamlFileLoader($container, new FileLocator($this->getProjectDir().'/app/config'));
-            $loader->load('sculpin_services.yml');
+            return $container;
         }
 
-        if (null !== $cont = $this->registerContainerConfiguration($this->getContainerLoader($container))) {
-            $container->merge($cont);
+        /**
+         * {@inheritdoc}
+         */
+        protected function initializeContainer(): void
+        {
+            $container = $this->buildContainer();
+            $container->set('kernel', $this);
+            $this->container = $container;
         }
 
-        return $container;
-    }
+        /**
+         * Get Sculpin bundles that were requested but were not found
+         *
+         * This can happen if a bundle is requested but has not been required and
+         * installed by Composer. Chances are this will lead to a lot of really bad
+         * things. This should be checked early by any Console applications to
+         * ensure that proper warnings are issued if there are any missing bundles
+         * detected.
+         *
+         * @return array
+         */
+        public function getMissingSculpinBundles(): array
+        {
+            return $this->missingSculpinBundles;
+        }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function initializeContainer(): void
-    {
-        $container = $this->buildContainer();
-        $container->set('kernel', $this);
-        $this->container = $container;
-    }
+        /**
+         * Gets the application root dir (path of the project's composer file).
+         *
+         * @return string
+         */
+        public function getProjectDir(): string
+        {
+            return $this->projectDir;
+        }
 
-    /**
-     * Get Sculpin bundles that were requested but were not found
-     *
-     * This can happen if a bundle is requested but has not been required and
-     * installed by Composer. Chances are this will lead to a lot of really bad
-     * things. This should be checked early by any Console applications to
-     * ensure that proper warnings are issued if there are any missing bundles
-     * detected.
-     *
-     * @return array
-     */
-    public function getMissingSculpinBundles(): array
-    {
-        return $this->missingSculpinBundles;
+        /**
+         * Get additional Sculpin bundles to register.
+         *
+         * @return string[] Fully qualified class names of the bundles to register.
+         */
+        abstract protected function getAdditionalSculpinBundles(): array;
     }
-
-    /**
-     * Gets the application root dir (path of the project's composer file).
-     *
-     * @return string
-     */
-    public function getProjectDir(): ?string
-    {
-        return $this->projectDir;
-    }
-
-    /**
-     * Get additional Sculpin bundles to register.
-     *
-     * @return string[] Fully qualified class names of the bundles to register.
-     */
-    abstract protected function getAdditionalSculpinBundles(): array;
+} else {
+    // PHP 8.1 + Symfony HttpKernel v6.4 results in AbstractKernel::getProjectDir's return type being "string",
+    // and not "?string". This bifurcation of AbstractKernel based on PHP version is quite unpleasant, but
+    // is just a temporary band-aid until Sculpin 4.x
+    require_once 'AbstractKernel.php81.php';
 }
